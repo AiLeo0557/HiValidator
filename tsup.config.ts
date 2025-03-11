d05df4123b7fcea9ac45f76f8844f1293e66e8d5
AiLeo0557 <leo1949@dingtalk.com>
2025-03-11 10:02:03 +0800

    chore: 更新 package.json 和 tsconfig.json 以支持 ES 模块和更高的目标版本，调整 tsup 配置以启用代码分割

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['esm', 'cjs'],
+  format: ['esm'],
   dts: true,
   clean: true,
   treeshake: true,
   sourcemap: true,
   minify: true,
+  splitting: true,
 });
